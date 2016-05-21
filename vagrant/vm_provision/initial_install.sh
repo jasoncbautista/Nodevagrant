6d1d6a4252df4c02b9bd5da17bbf290df3cdd2d9
Jason Bautista <bjason@gmail.com>
2016-05-21 16:04:12 -0700

    Provision

---
+++ vagrant/vm_provision/initial_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Hello, updating stuff:"
+apt-get update
+echo "Hello, installing stuff.."
+
+apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_4.x | sh
+apt-get install -y nodejs
+
+
+
