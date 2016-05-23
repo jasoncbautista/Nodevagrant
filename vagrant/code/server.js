df44ed53fc6bc26e9997a2f37f56024a24593890
Jason Bautista <bjason@gmail.com>
2016-05-22 20:09:15 -0700

    Start dev added so we can run with: npm run start:dev

--- vagrant/code/server.js
+++ vagrant/code/server.js
@@ -4,7 +4,8 @@ var app = express();
 app.use('/static', express.static('public'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  const wassa = 'wassa'
+  res.send('Hello World 3!', wassa);
 });
 
 app.listen(3000, function() {
