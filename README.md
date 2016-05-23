af645d1760f87b6b9e41b13edbf4cb33d1c5c60e
Jason Bautista <bjason@gmail.com>
2016-05-22 20:10:50 -0700

    Constants working

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # blah blah blah
 npm install --save-dev babel-preset-es2015
 nodemon -L index.js --exec babel-node --presets es2015,stage-2
+
+
+
+Run dev:
+```
+  npm run start:dev
+```
--- vagrant/code/server.js
+++ vagrant/code/server.js
@@ -4,8 +4,8 @@ var app = express();
 app.use('/static', express.static('public'));
 
 app.get('/', function(req, res) {
-  const wassa = 'wassa'
-  res.send('Hello World 3!', wassa);
+  const wassa = 'wassa';
+  res.send('Hello World 3! ' + wassa);
 });
 
 app.listen(3000, function() {
