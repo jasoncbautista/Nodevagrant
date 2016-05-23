416f63daa417dfa92e9a87a887446d39eb39bb13
Jason Bautista <bjason@gmail.com>
2016-05-22 20:05:54 -0700

    Node 5 and fixing stage2 thing

--- vagrant/vm_provision/initial_install.sh
+++ vagrant/vm_provision/initial_install.sh
@@ -5,7 +5,7 @@ apt-get update
 echo "Hello, installing stuff.."
 
 apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_4.x | sh
+curl -sL https://deb.nodesource.com/setup_5.x | sh
 apt-get install -y nodejs
 # su vagrant
 npm install -g nodemon
